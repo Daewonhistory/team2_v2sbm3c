e4d6600f77cef78e24307ad855cd19b6f25614d8
inhovation <inhovation423@gmail.com>
2024-06-13 17:55:55 +0900

    0613 중간발표 머지

--- src/main/java/dev/mvc/tool/SmsTool.java
+++ src/main/java/dev/mvc/tool/SmsTool.java
@@ -34,7 +34,7 @@ public class SmsTool {
 
 
   public SmsTool() {
-    this.messageService = NurigoApp.INSTANCE.initialize("", "", "https://api.coolsms.co.kr");
+    this.messageService = NurigoApp.INSTANCE.initialize("NCSXCGGNU2ETCFIN", "LSPWXJZOELE2WUSROBEHNZFCSCB8URXR", "https://api.coolsms.co.kr");
   }
 
   @Async
